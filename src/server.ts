6fdc64b6832e7e4063b81b0df02670f0fe015ac9
chudah <yakung25c@gmail.com>
2022-08-06 16:01:31 +0000

    Updated

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   const app = express();
 
   // Set the network port
-  const port = process.env.port || 8000;
+  const port = process.env.PORT || 8081;
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
